3ca04044ee16d43a7180202c1b710221acd6f0f7
shuja hyder <shujamalik04@gmail.com>
2025-03-08 21:35:46 +0500

    .

--- Frontend/components/Layout/UserLayout.jsx
+++ Frontend/components/Layout/UserLayout.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Header from '../Common/Header'
+import Footer from '../Common/Footer'
 
 const UserLayout = () => {
   return (
@@ -8,6 +9,7 @@ const UserLayout = () => {
     <Header/>
    {/*Main Content*/}
    {/*Footer*/}
+    <Footer/>
 
    
    </>
